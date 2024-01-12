dbc67d9cb4c9ac3ad3198619cb04981a1344199b
LuizSeabraDeMarco <139667471+LuizSeabraDeMarco@users.noreply.github.com>
2024-01-12 15:37:15 -0300

    Adicionando arquivos

---
+++ menu.js
@@ -0,0 +1,11 @@
+function link_home(){
+    window.location.href = "index.html";
+}
+
+function link_contato(){
+    window.location.href = "https://wa.me/5524993145325";
+}
+
+function link_sobre_n(){
+    window.location.href = "sobre_nos.html";
+}
---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+function link_insta(){
+    window.location.href = "https://www.instagram.com/tampet_sulfluminense/";
+}
+
+function voltar() {
+    history.back();
+  }
+  
